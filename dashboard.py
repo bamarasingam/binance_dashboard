a60fb2bea864f8b300d9004f7380ccfb8670980b
Brandon <bamarasingam@gmail.com>
2024-07-09 00:31:46 -0400

    updated

--- dashboard.py
+++ dashboard.py
@@ -100,67 +100,71 @@ def create_chart(df, symbol):
 
 #---------------------------------------------------Streamlit-------------------------------------------------
 
-#Centered title
-st.markdown("<h2 style='text-align: center;'>Crypto Technical Analysis Dashboard</h2>", unsafe_allow_html=True)
+#Create tabs for dashboard
+tab1, tab2 = st.tabs(["Data/Analytics", "ML Predictions"])
 
-#Sidebar Components
-symbol = st.sidebar.text_input("Crypto Symbol (e.g. BTCUSDT)", "BTCUSDT")
-intervals = ('1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M')
-default_index = intervals.index('1d')
-interval = st.sidebar.selectbox("Interval", 
-                                options=intervals,
-                                index=default_index)
+with tab1:
+    #Centered title
+    st.markdown("<h2 style='text-align: center;'>Crypto Technical Analysis Dashboard</h2>", unsafe_allow_html=True)
 
-#Date input for start date
-default_date = datetime.now().date() - timedelta(days=365)
-start_date = st.sidebar.date_input("Start Date", 
-                                   value= default_date,
-                                   max_value=datetime.now())
-show_data = st.sidebar.checkbox(label="Show Data", value = True)
-show_chart = st.sidebar.checkbox(label="Show Chart", value = True)
+    #Sidebar Components
+    symbol = st.sidebar.text_input("Crypto Symbol (e.g. BTCUSDT)", "BTCUSDT")
+    intervals = ('1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M')
+    default_index = intervals.index('1d')
+    interval = st.sidebar.selectbox("Interval", 
+                                    options=intervals,
+                                    index=default_index)
 
-df = load_data(symbol, interval, start_date)
-reversed_df = df.iloc[::-1] #Reversed dataframe to be shown in Streamlit
-row1_val = reversed_df.iloc[0]['close']
-ema20_val = reversed_df.iloc[0]['EMA_20']
-ema200_val = reversed_df.iloc[0]['EMA_200']
-rsi_val = reversed_df.iloc[0]['RSI_14']
-adx = reversed_df.iloc[0]['ADX_14']
-dmp = reversed_df.iloc[0]['DMP_14']
-dmn = reversed_df.iloc[0]['DMN_14']
+    #Date input for start date
+    default_date = datetime.now().date() - timedelta(days=365)
+    start_date = st.sidebar.date_input("Start Date", 
+                                    value= default_date,
+                                    max_value=datetime.now())
+    show_data = st.sidebar.checkbox(label="Show Data", value = True)
+    show_chart = st.sidebar.checkbox(label="Show Chart", value = True)
 
-row20_val = reversed_df.iloc[20]['close'] if len(reversed_df) > 20 else row1_val
-row60_val = reversed_df.iloc[60]['close'] if len(reversed_df) > 60 else row1_val
-row120_val = reversed_df.iloc[120]['close'] if len(reversed_df) > 120 else row1_val
-row240_val = reversed_df.iloc[240]['close'] if len(reversed_df) > 240 else row1_val
+    df = load_data(symbol, interval, start_date)
+    reversed_df = df.iloc[::-1] #Reversed dataframe to be shown in Streamlit
+    row1_val = reversed_df.iloc[0]['close']
+    ema20_val = reversed_df.iloc[0]['EMA_20']
+    ema200_val = reversed_df.iloc[0]['EMA_200']
+    rsi_val = reversed_df.iloc[0]['RSI_14']
+    adx = reversed_df.iloc[0]['ADX_14']
+    dmp = reversed_df.iloc[0]['DMP_14']
+    dmn = reversed_df.iloc[0]['DMN_14']
 
-day20_ret_percent = (row1_val - row20_val)/row20_val * 100
-day60_ret_percent = (row1_val - row60_val)/row60_val * 100
-day120_ret_percent = (row1_val - row120_val)/row120_val * 100
-day240_ret_percent = (row1_val - row240_val)/row240_val * 100
+    row20_val = reversed_df.iloc[20]['close'] if len(reversed_df) > 20 else row1_val
+    row60_val = reversed_df.iloc[60]['close'] if len(reversed_df) > 60 else row1_val
+    row120_val = reversed_df.iloc[120]['close'] if len(reversed_df) > 120 else row1_val
+    row240_val = reversed_df.iloc[240]['close'] if len(reversed_df) > 240 else row1_val
 
-#Column wise Display
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.subheader("Returns")
-    st.markdown(f"- 1  MONTH : {round(day20_ret_percent,2)}% {get_returns_emoji(round(day20_ret_percent,2))}")
-    st.markdown(f"- 3  MONTHS : {round(day60_ret_percent,2)}% {get_returns_emoji(round(day60_ret_percent,2))}")
-    st.markdown(f"- 6  MONTHS : {round(day120_ret_percent,2)}% {get_returns_emoji(round(day120_ret_percent,2))}")
-    st.markdown(f"- 12 MONTHS : {round(day240_ret_percent,2)}% {get_returns_emoji(round(day240_ret_percent,2))}")
-with col2:
-    st.subheader("Momentum")
-    st.markdown(f"- LTP : {round(row1_val,2)}") 
-    st.markdown(f"- EMA20 : {round(ema20_val,2)} {get_ema_emoji(round(row1_val,2),round(ema20_val,2))}") 
-    st.markdown(f"- EMA200 : {round(ema200_val,2)} {get_ema_emoji(round(row1_val,2),round(ema200_val,2))}") 
-    st.markdown(f"- RSI : {round(rsi_val,2)} {get_rsi_emoji(round(rsi_val,2))}") 
-with col3:
-    st.subheader("Trend Strength")
-    st.markdown(f"- ADX : {round(adx,2)} {get_adx_emoji(round(adx,2))}") 
-    st.markdown(f"- DMP : {round(dmp,2)} ") 
-    st.markdown(f"- DMN : {round(dmn,2)} ") 
+    day20_ret_percent = (row1_val - row20_val)/row20_val * 100
+    day60_ret_percent = (row1_val - row60_val)/row60_val * 100
+    day120_ret_percent = (row1_val - row120_val)/row120_val * 100
+    day240_ret_percent = (row1_val - row240_val)/row240_val * 100
 
-if show_chart:
-    st.plotly_chart(create_chart(df, symbol))
+    #Column wise Display
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.subheader("Returns")
+        st.markdown(f"- 1  MONTH : {round(day20_ret_percent,2)}% {get_returns_emoji(round(day20_ret_percent,2))}")
+        st.markdown(f"- 3  MONTHS : {round(day60_ret_percent,2)}% {get_returns_emoji(round(day60_ret_percent,2))}")
+        st.markdown(f"- 6  MONTHS : {round(day120_ret_percent,2)}% {get_returns_emoji(round(day120_ret_percent,2))}")
+        st.markdown(f"- 12 MONTHS : {round(day240_ret_percent,2)}% {get_returns_emoji(round(day240_ret_percent,2))}")
+    with col2:
+        st.subheader("Momentum")
+        st.markdown(f"- LTP : {round(row1_val,2)}") 
+        st.markdown(f"- EMA20 : {round(ema20_val,2)} {get_ema_emoji(round(row1_val,2),round(ema20_val,2))}") 
+        st.markdown(f"- EMA200 : {round(ema200_val,2)} {get_ema_emoji(round(row1_val,2),round(ema200_val,2))}") 
+        st.markdown(f"- RSI : {round(rsi_val,2)} {get_rsi_emoji(round(rsi_val,2))}") 
+    with col3:
+        st.subheader("Trend Strength")
+        st.markdown(f"- ADX : {round(adx,2)} {get_adx_emoji(round(adx,2))}") 
+        st.markdown(f"- DMP : {round(dmp,2)} ") 
+        st.markdown(f"- DMN : {round(dmn,2)} ") 
 
-if show_data:
-    st.write(reversed_df)
+    if show_chart:
+        st.plotly_chart(create_chart(df, symbol))
+
+    if show_data:
+        st.write(reversed_df)
